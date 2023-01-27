682193e4a93f663654be4b1c51314f8e19e8bc36
Mr. SMH <77523793+SMojtabaHosseini@users.noreply.github.com>
2023-01-27 17:40:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print("I'm Live as {0.user}".format(client))
+
   
 def getGas():
-  apiKey = os.getenv('ApiKey')
-  response = requests.get("https://api.etherscan.io/api
-                          ?module=gastracker
-                          &action=gasoracle
-                          &apikey={0}".format(apiKey))
-  return response
+  apiKey = os.getenv('API_KEY')
+  response = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={0}".format(apiKey))
+  json_data = json.loads(response.text)
+  return json_data['result']
                         
                           
 @client.event
@@ -22,9 +21,15 @@ async def on_message(message):
     return
   
   if message.content == "!gas":
-    gas = getGas()
-    json_data = json.load(gas
+    result= getGas()
+    safe = result["SafeGasPrice"]
+    propose = result["ProposeGasPrice"]
+    fast = result["FastGasPrice"]
+    gas = "Safe Gas Price: " + safe + "\nPropose Gas Price: " + propose + "\nFast Gas Price: " + fast
+
+    await message.channel.send(gas)
     
     
-////get the token bot from .env file and put it in the run method.
+    
+## get the token bot from .env file and put it in the run method.
 client.run(os.getenv('TOKEN'))
