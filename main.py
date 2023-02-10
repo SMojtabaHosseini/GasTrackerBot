d103b8f995646371c5647a33eafeaa87ef9727f4
Mr. SMH <77523793+SMojtabaHosseini@users.noreply.github.com>
2023-02-10 06:06:57 +0330

    Update main.py
    polygon added and some bugs fixed

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-import discord, os, requests, json
+import discord
+import os
+import requests
+import json
 
-
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
   print("I'm Live as {0.user}".format(client))
 
   
-def getGas():
-  apiKey = os.getenv('API_KEY')
-  response = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={0}".format(apiKey))
+def getEthGas():
+  apiKey = os.getenv('ETH_API_KEY')
+  response = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + apiKey)
+  json_data = json.loads(response.text)
+  print (json_data['result'])
+  return json_data['result']
+
+def getPolygonGas():
+  apiKey = os.getenv('POLYGON_API_KEY')
+  response = requests.get("https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" + apiKey))
   json_data = json.loads(response.text)
   return json_data['result']
+
+def setResult(result):
+  safe = result["SafeGasPrice"]
+  propose = result["ProposeGasPrice"]
+  fast = result["FastGasPrice"]
+  gas = "Safe Gas Price: " + safe + "\nPropose Gas Price: " + propose + "\nFast Gas Price: " + fast
+  return gas
                         
                           
 @client.event
@@ -20,16 +38,16 @@ async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content == "!gas":
-    result= getGas()
-    safe = result["SafeGasPrice"]
-    propose = result["ProposeGasPrice"]
-    fast = result["FastGasPrice"]
-    gas = "Safe Gas Price: " + safe + "\nPropose Gas Price: " + propose + "\nFast Gas Price: " + fast
-
+  if message.content.lower() == "!gas" or message.content.lower() == "!gas eth":
+    result = getEthGas()
+    gas = setResult(result)
+    await message.channel.send(gas)
+    
+  if message.content.lower() == "!gas polygon":
+    result = getPolygonGas()
+    gas = setResult(result)
     await message.channel.send(gas)
     
     
-    
 ## get the token bot from .env file and put it in the run method.
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('BOT_TOKEN'))
