b2e9d59919e394f2d68081d00dab47ad2765a7b4
Mr. SMH <77523793+SMojtabaHosseini@users.noreply.github.com>
2023-02-11 06:51:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 import requests
 import json
+import time
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,20 +18,26 @@ def getEthGas():
   apiKey = os.getenv('ETH_API_KEY')
   response = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + apiKey)
   json_data = json.loads(response.text)
-  print (json_data['result'])
   return json_data['result']
 
 def getPolygonGas():
   apiKey = os.getenv('POLYGON_API_KEY')
-  response = requests.get("https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" + apiKey))
+  response = requests.get("https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" + apiKey)
   json_data = json.loads(response.text)
   return json_data['result']
 
-def setResult(result):
+
+
+def embedMaker(result, chain):
   safe = result["SafeGasPrice"]
   propose = result["ProposeGasPrice"]
   fast = result["FastGasPrice"]
-  gas = "Safe Gas Price: " + safe + "\nPropose Gas Price: " + propose + "\nFast Gas Price: " + fast
+  
+  description = "\n**:turtle:  |  Slow :**\t"+safe+"\n\n**:person_walking:  |  Avarage:**\t"+propose+"\n\n**:person_running:  |  Fast:**\t"+fast
+  
+  gas = discord.Embed(title=":fuelpump: {0} Gas Price".format(chain), description=description, color=0x29252b)
+  gas.set_footer(text="\npowered by {0}scan".format(chain))
+  
   return gas
                         
                           
@@ -40,14 +48,25 @@ async def on_message(message):
   
   if message.content.lower() == "!gas" or message.content.lower() == "!gas eth":
     result = getEthGas()
-    gas = setResult(result)
-    await message.channel.send(gas)
+    gas = embedMaker(result,"Ether")
+    await message.reply(embed=gas)
     
   if message.content.lower() == "!gas polygon":
     result = getPolygonGas()
-    gas = setResult(result)
-    await message.channel.send(gas)
+    gas = embedMaker(result, "Polygon")
+    await message.reply(embed=gas)
     
     
 ## get the token bot from .env file and put it in the run method.
-client.run(os.getenv('BOT_TOKEN'))
+keep_alive()
+try:
+  client.run(os.getenv("BOT_TOKEN"))
+except discord.HTTPException as e:
+  if e.status == 429:
+    print("The Discord servers denied the connection for making too many requests")
+    print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
+    os.system("python restarter.py")
+    os.system('kill 1')
+        #print("Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests")
+  else:
+    raise e
